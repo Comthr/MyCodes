2483ad28bc664a9916998a948ae64fbfaf5e6bcb
Comthr <1312380124@qq.com>
2025-07-27 18:51:41 +0800

    Update Singleton.cs

--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using UnityEngine;
 namespace Singleton
 {
-    #region 饿汉式Singleton
+    #region 懒汉式Singleton
     public class Singleton<T> where T : Singleton<T>
     {
         private static T m_Instance = null;
@@ -16,41 +16,65 @@ namespace Singleton
                     lock (m_Lock)
                     {
                         if (m_Instance == null)
-                            m_Instance = (Array.Find(typeof(T).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic),
-                                (ConstructorInfo c) => c.GetParameters().Length == 0) ??
-                                throw new Exception("没有找到非公共的无参构造函数！")).Invoke(null) as T;
+                        {
+                            var constructors = typeof(T).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic);
+                            var constructor = Array.Find(constructors, (ConstructorInfo c) => c.GetParameters().Length == 0)
+                                ?? throw new Exception("没有找到非公共的无参构造函数！");
+                            m_Instance = constructor.Invoke(null) as T;
+                        }
                     }
                 return m_Instance;
             }
         }
         public virtual void Init() { }
-        protected virtual void OnDestroy()
-        {
-            m_Instance = null;
-        }
     }
     #endregion
-    #region 饿汉式MonoSingleton
+    #region 静态内部类
+    /// <summary>
+    /// 通过内部静态类来保证单例的唯一性，也属于懒加载，并且是线程安全的
+    /// </summary>
+    public class SingletonHolder<T> where T : SingletonHolder<T>
+    {
+        private SingletonHolder() { }​
+        public static T i { get { return Holder.i; } }
+        private static class Holder
+        {
+            public static readonly T i = GetInstance();
+            private static T GetInstance()
+            {
+                var constructors = typeof(T).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic);
+                var constructor = Array.Find(constructors, (ConstructorInfo c) => c.GetParameters().Length == 0)
+                    ?? throw new Exception("没有找到非公共的无参构造函数！");
+                return constructor?.Invoke(null) as T;
+            }
+        }​
+    }
+    #endregion
+    #region 懒汉式MonoSingleton
     public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
     {
         private static T m_Instance;
+        private static readonly string m_Lock = "lock";
         public static T i
         {
             get
             {
                 if (m_Instance == null)
-                {
-                    var name = $"[{typeof(T)}]";
-                    var flag = Global.Singletons.Contains(name);//查询是否注册过
-                    if (!flag)
+                    lock (m_Lock)
                     {
-                        m_Instance = new GameObject(name, typeof(T)).GetComponent<T>();
-                        Global.Singletons.Add(name);
+                        if (m_Instance == null)
+                        {
+                            var name = $"[{typeof(T)}]";
+                            var flag = Global.Singletons.Contains(name);//查询是否注册过
+                            if (!flag)
+                            {
+                                m_Instance = new GameObject(name, typeof(T)).GetComponent<T>();
+                                Global.Singletons.Add(name);
+                            }
+                        }
                     }
-                }
                 return m_Instance;
             }
-        }
         public virtual void Init() { }
         protected virtual void OnDestroy()
         {
